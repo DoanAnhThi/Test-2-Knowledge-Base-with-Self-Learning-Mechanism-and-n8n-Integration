894d417b67d4ecc017e8d7b879666addb9c923b1
AnhThiPro <bincalam28499@gmail.com>
2025-03-04 05:15:35 +0700

    feedback lên supabase đi mà

--- RAG-n8n.py
+++ RAG-n8n.py
@@ -15,38 +15,43 @@ upload_API_URL = "https://bincalam28499.app.n8n.cloud/webhook-test/post-to-Googl
 
 
 # GIAO DIỆN TRANG WEB
-def main():
-    st.title("Hỏi đáp với AI")
+# def main():
+st.title("Hỏi đáp với AI")
 
-    # Ô upload file
-    uploaded_file = st.file_uploader("Tải lên tệp PDF, Word hoặc Text", type=["pdf", "docx", "txt"])
 
-    question = st.text_input("Đặt câu hỏi của bạn:")
+# Ô upload file
+uploaded_file = st.file_uploader("Tải lên tệp PDF, Word hoặc Text", type=["pdf", "docx", "txt"])
+if uploaded_file:
+    # Tải file lên google drive
+    response = requests.post(upload_API_URL, data={'filename':uploaded_file.name}, files ={'file': (uploaded_file.name, uploaded_file, uploaded_file.type)})
+    st.write("Bạn đã tải lên:", uploaded_file.name, "vui lòng chờ vài giây để kiến thức được cập nhật")
+
+
+question = st.text_input("Đặt câu hỏi của bạn:")
+st.write(question)
+if question:
+    # Gửi question lên Webhook
+    response = requests.post(question_API_URL, json={'question':question})
     
-    if question:
-        # Gửi question lên Webhook
-        response = requests.post(question_API_URL, json={'question':question})
-        
-        # Lấy về câu trả lời
-        ai_response = response.json()
-        output_text = ai_response.get("output", "Không có dữ liệu")
+    # Lấy về câu trả lời
+    ai_response = response.json()
+    output_text = ai_response.get("output", "Không có dữ liệu")
 
-        st.write("Câu trả lời:",output_text)  # Xem câu trả lời của AI Agent
+    st.write("Câu trả lời:",output_text)  # Xem câu trả lời của AI Agent
 
     # Ô feedback 
     feedback = st.text_input("Bạn có hài hong với câu trả lời này không")
     # Đẩy dữ liệu lên vector DB
-    if feedback:
-        # Gửi feedback lên Webhook
-        response = requests.post(feedback__API_URL, json={'feedback':feedback})
-        st.write('Cảm ơn bạn đã gửi phản hồi')
-    
-    if uploaded_file:
-        # Tải file lên google drive
-        response = requests.post(upload_API_URL, data={'filename':uploaded_file.name}, files ={'file': (uploaded_file.name, uploaded_file, uploaded_file.type)})
-        st.write("Bạn đã tải lên:", uploaded_file.name, "vui lòng chờ vài giây để kiến thức được cập nhật")
+if feedback:
+    feedback_prompt = f"Câu hỏi:' {question} ' không hề khó như bạn nghĩ '{question}' được trả lời như sau:' {feedback}'"
+
+    # Gửi feedback lên Webhook
+    response = requests.post(feedback__API_URL, json={'feedback_prompt':feedback_prompt})
+    # st.write('Cảm ơn bạn đã gửi phản hồi')
+    st.write(feedback_prompt)
+
 
     
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
 
