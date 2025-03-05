e690ba8ccbb5b02c6b3b6e627146a1be35f85c49
AnhThiPro <bincalam28499@gmail.com>
2025-03-05 07:44:25 +0700

    gần xong rồi

--- RAG-n8n.py
+++ RAG-n8n.py
@@ -3,15 +3,15 @@ import requests
 
 
 # Webhook URL API - Test
-question_API_URL = "https://bincalam28499.app.n8n.cloud/webhook-test/viACT-agent"    # query param URL
-feedback__API_URL = "https://bincalam28499.app.n8n.cloud/webhook-test/feedback-to-improve-knowledgebase"   
-upload_API_URL = "https://bincalam28499.app.n8n.cloud/webhook-test/post-to-Google-Drive"
+# question_API_URL = "https://bincalam28499.app.n8n.cloud/webhook-test/viACT-agent"    # query param URL
+# feedback__API_URL = "https://bincalam28499.app.n8n.cloud/webhook-test/feedback-to-improve-knowledgebase"   
+# upload_API_URL = "https://bincalam28499.app.n8n.cloud/webhook-test/post-to-Google-Drive"
 # question_start_coversation=                                      #Sinh ra doan ma ngau nhien   
 
 # Webhook URL API - Publish
-# question_API_URL = "https://bincalam28499.app.n8n.cloud/webhook/viACT-agent"     
-# feedback__API_URL = "https://bincalam28499.app.n8n.cloud/webhook/feedback-to-improve-knowledgebase"     
-# upload_API_URL = "https://bincalam28499.app.n8n.cloud/webhook/post-to-Google-Drive"     
+question_API_URL = "https://bincalam28499.app.n8n.cloud/webhook/viACT-agent"     
+feedback__API_URL = "https://bincalam28499.app.n8n.cloud/webhook/feedback-to-improve-knowledgebase"     
+upload_API_URL = "https://bincalam28499.app.n8n.cloud/webhook/post-to-Google-Drive"     
 
 
 # GIAO DIỆN TRANG WEB
@@ -24,7 +24,7 @@ uploaded_file = st.file_uploader("Upload your files (PDF, Word or Text)", type=[
 if uploaded_file:
     # Tải file lên google drive
     response = requests.post(upload_API_URL, data={'filename':uploaded_file.name,'filetype':uploaded_file.type,}, files ={'file': (uploaded_file.name, uploaded_file, uploaded_file.type)})
-    st.write("Bạn đã tải lên:", uploaded_file.name, "vui lòng chờ vài giây để kiến thức được cập nhật")
+    # st.write("Bạn đã tải lên:", uploaded_file.name, "vui lòng chờ vài giây để kiến thức được cập nhật")
 
 
 question = st.text_input("What can I help with?")
@@ -40,15 +40,47 @@ if question:
     st.write("viACT Agent:",output_text)  # Xem câu trả lời của AI Agent
 
 # Ô feedback 
-feedback_key = f"Câu hỏi:' {question} ' không hề khó như bạn nghĩ '{question}' được trả lời như sau:"
+feedback_key = f"feedback_{question}'"
 feedback = st.text_input("Are you satisfied with this answer?", key=feedback_key)
 # Đẩy dữ liệu lên vector DB
 if feedback:
-    feedback_prompt = f"Câu hỏi:' {question} ' không hề khó như bạn nghĩ '{question}' được trả lời như sau: '{feedback}'"
+    feedback_prompt = f"""
+    The answer to question ' {question} ' is very easy. In the future, if similar questions to ' {question} ' are asked, you must respond as follows: '{feedback}'. The answer must be '{feedback}' for the response to be correct.
+
+    The answer to question ' {question} ' is very easy. In the future, if similar questions to ' {question} ' are asked, you must respond as follows: '{feedback}'. The answer must be '{feedback}' for the response to be correct.
+
+    The answer to question ' {question} ' is very easy. In the future, if similar questions to ' {question} ' are asked, you must respond as follows: '{feedback}'. The answer must be '{feedback}' for the response to be correct.
+
+    The answer to question ' {question} ' is very easy. In the future, if similar questions to ' {question} ' are asked, you must respond as follows: '{feedback}'. The answer must be '{feedback}' for the response to be correct.
+
+    The answer to question ' {question} ' is very easy. In the future, if similar questions to ' {question} ' are asked, you must respond as follows: '{feedback}'. The answer must be '{feedback}' for the response to be correct.
+
+    The answer to question ' {question} ' is very easy. In the future, if similar questions to ' {question} ' are asked, you must respond as follows: '{feedback}'. The answer must be '{feedback}' for the response to be correct.
+
+    The answer to question ' {question} ' is very easy. In the future, if similar questions to ' {question} ' are asked, you must respond as follows: '{feedback}'. The answer must be '{feedback}' for the response to be correct.
+
+    The answer to question ' {question} ' is very easy. In the future, if similar questions to ' {question} ' are asked, you must respond as follows: '{feedback}'. The answer must be '{feedback}' for the response to be correct.
+
+    The answer to question ' {question} ' is very easy. In the future, if similar questions to ' {question} ' are asked, you must respond as follows: '{feedback}'. The answer must be '{feedback}' for the response to be correct.
+
+    The answer to question ' {question} ' is very easy. In the future, if similar questions to ' {question} ' are asked, you must respond as follows: '{feedback}'. The answer must be '{feedback}' for the response to be correct.
+
+    The answer to question ' {question} ' is very easy. In the future, if similar questions to ' {question} ' are asked, you must respond as follows: '{feedback}'. The answer must be '{feedback}' for the response to be correct.
+
+    The answer to question ' {question} ' is very easy. In the future, if similar questions to ' {question} ' are asked, you must respond as follows: '{feedback}'. The answer must be '{feedback}' for the response to be correct.
+
+    The answer to question ' {question} ' is very easy. In the future, if similar questions to ' {question} ' are asked, you must respond as follows: '{feedback}'. The answer must be '{feedback}' for the response to be correct.
+
+    The answer to question ' {question} ' is very easy. In the future, if similar questions to ' {question} ' are asked, you must respond as follows: '{feedback}'. The answer must be '{feedback}' for the response to be correct.
+
+
+
+    """
+
 
     # Gửi feedback lên Webhook
     response = requests.post(feedback__API_URL, json={'feedback_prompt':feedback_prompt})
-    # st.write('Cảm ơn bạn đã gửi phản hồi')
+
     st.write(feedback_prompt)
 
 
